47c5683d4e1408b6d8e8a1ecabcbd0533de7a22b
Kadee80 <kaa328@nyu.edu>
2016-01-07 13:35:28 -0500

    initial commit

---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'font-awesome-sass'
+preferred_syntax = :scss
+http_path = '/'
+css_path = 'static/css'
+sass_path = 'sass'
+images_path = 'static/img'
+javascripts_path = 'static/js'
+relative_assets = true
+line_comments = true
+# output_style = :compressed
